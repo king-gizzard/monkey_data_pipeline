9f0ad1e390ef57abf64bdd83b929ae72a9eebddb
king-gizzard <flo.meinecke@gmail.com>
2021-05-04 23:46:51 +0200

    now uses rect sheet, not cont

--- python/make_protocols_list.py
+++ python/make_protocols_list.py
@@ -18,17 +18,19 @@ for xlsx in raw_files:
     print(xlsx)
     src = get_file_path(xlsx)
     #open file from ALL file list
-    cont = pd.read_excel(src,sheet_name='Cont')
+    if not has_rect_sheet(src):
+        continue
+    rect = pd.read_excel(src,sheet_name='Rect')
     head = pd.read_excel(src,sheet_name='Header',header=None)
     date = head.iloc[0,1]
     time = head.iloc[1,1]
     focal = head.iloc[2,1]
     #ignore files with default 'unknown monkey' as focal
     #as those have not been used for data collection
-    if focal == 'umo':
-        continue
+#    if focal == 'umo':
+#        continue
     #ignore files which would be empty after time&actor-nan-removal
     #i.e. empty protocol which had a focal animal entered and nothing else.
-    if all(cont['Time'].isna()) and all(cont['Action'].isna()):
-        continue
-    update_csv(focal_info,protocols_csv_path,[xlsx,date,time,focal,list(cont['Time'])[-1]])
+#    if all(rect['Time'].isna()) and all(rect['Action'].isna()):
+#        continue
+    update_csv(focal_info,protocols_csv_path,[xlsx,date,time,focal,list(rect['Time'])[-1]])
